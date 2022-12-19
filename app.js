50c7d6f85d79f0ffaa496fad7716824c73a72b36
BoRuei1028 <andy6o43222@gmail.com>
2022-12-19 22:20:08 +0800

    feat: add views / layout

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const { mainModule } = require('process')
+const app = express()
+const Restaurant = require('./models/restaurant') //載入model
+const restaurant = require('./models/restaurant')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const app = express()
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.static('public'))
 
-// DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7. Use `mongoose.set('strictQuery', false);` if you want to prepare for this change.Or use`mongoose.set('strictQuery', true);` to suppress this warning.
 mongoose.set('strictQuery', false)
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -24,7 +26,17 @@ db.once('open', () => {
   console.log('mongodb connected')
 })
 app.get('/', (req, res) => {
-  res.render('index')
+  Restaurant.find()
+    .lean()
+    .then(restaurant => res.render('index', { restaurantData: restaurant }))
+    .catch(error => console.log(error))
+})
+app.get('/restaurant/:restaurant_id', (req, res) => {
+  const id = req.params.restaurant_id
+  return Restaurant.findById(id)
+    .lean()
+    .then(item => res.render('detail', { item }))
+    .catch(error => console.log(error))
 })
 
 app.listen(3000)
