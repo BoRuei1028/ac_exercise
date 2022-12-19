58ab7509b443d07552d0cdaa41c235d39d03cc53
BoRuei1028 <andy6o43222@gmail.com>
2022-12-19 15:53:06 +0800

    feat: add hamdlebars

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
+const exphbs = require('express-handlebars')
+const { mainModule } = require('process')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 const app = express()
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 // DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7. Use `mongoose.set('strictQuery', false);` if you want to prepare for this change.Or use`mongoose.set('strictQuery', true);` to suppress this warning.
-mongoose.set('strictQuery', false) 
+mongoose.set('strictQuery', false)
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const port = 3000
@@ -21,7 +24,7 @@ db.once('open', () => {
   console.log('mongodb connected')
 })
 app.get('/', (req, res) => {
-  res.send('OK')
+  res.render('index')
 })
 
 app.listen(3000)
