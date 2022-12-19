5e3b15a288ce92b37d006263177ea92fc30a5f75
BoRuei1028 <andy6o43222@gmail.com>
2022-12-19 14:51:42 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
+
+// DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7. Use `mongoose.set('strictQuery', false);` if you want to prepare for this change.Or use`mongoose.set('strictQuery', true);` to suppress this warning.
+mongoose.set('strictQuery', false) 
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
 const port = 3000
 
-app.get('/', (req,res) => {
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error')
+})
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+app.get('/', (req, res) => {
   res.send('OK')
 })
 
