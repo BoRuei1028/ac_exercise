39465e29dcdfad56674cd49d3f0ee6b75dda3eef
BoRuei1028 <andy6o43222@gmail.com>
2022-12-20 11:42:29 +0800

    feat: add new function

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const app = express()
 const Restaurant = require('./models/restaurant') //載入model
-const restaurant = require('./models/restaurant')
-
+// const restaurant = require('./models/restaurant')
+const port = 3000
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -12,11 +12,12 @@ if (process.env.NODE_ENV !== 'production') {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 mongoose.set('strictQuery', false)
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-const port = 3000
+
 
 const db = mongoose.connection
 db.on('error', () => {
@@ -25,12 +26,20 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected')
 })
+
+// 瀏覽全部
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurant => res.render('index', { restaurantData: restaurant }))
     .catch(error => console.log(error))
 })
+// 新增餐廳
+app.get('/restaurant/new', (req, res) => {
+  res.render('new')
+})
+
+// 瀏覽特定餐廳
 app.get('/restaurant/:restaurant_id', (req, res) => {
   const id = req.params.restaurant_id
   return Restaurant.findById(id)
@@ -39,4 +48,11 @@ app.get('/restaurant/:restaurant_id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// 新增餐廳to資料庫
+app.post('/restaurant', (req, res) => {
+  const restaurantData = req.body
+  Restaurant.create(restaurantData)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 app.listen(3000)
