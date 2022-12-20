19545f39418d592d611b22ba0a98f56b5d661b19
BoRuei1028 <andy6o43222@gmail.com>
2022-12-20 22:32:12 +0800

    feat: add search function

--- app.js
+++ app.js
@@ -53,6 +53,29 @@ app.get('/restaurant/:restaurant_id/edit', (req, res) => {
     .then((restaurantDataById) => res.render('edit', { item: restaurantDataById }))
     .catch(error => console.log(error))
 })
+// 搜尋功能
+app.get('/search', (req, res) => {
+
+  if (!req.query.keyword) {
+    return res.redirect('/')
+  }
+
+  const userInputKeyword = req.query.keyword
+  const keyword = req.query.keyword.trim().toLowerCase() //搜尋欄name="keyword"
+  Restaurant.find({})
+    .lean()
+    .then((restaurantData) => {
+      const filterData = restaurantData.filter(restaurant => restaurant.name.toLowerCase().includes(keyword) || restaurant.category.includes(keyword)
+      )
+      let searchResult = filterData.length
+      if (!filterData.length) {
+        searchResult = "0" // 給數字0的話，不會顯示出alert
+        return res.render('index', { restaurantData, keyword: userInputKeyword, resultCount: searchResult })
+      }
+      res.render('index', { restaurantData: filterData, keyword: userInputKeyword, resultCount: searchResult })
+    })
+    .catch(error => console.log(error))
+})
 
 app.post('/restaurant', (req, res) => {
   const restaurantData = req.body
