29974bab04e8cdaa411f8ede9afaae422a1946a8
BoRuei1028 <andy6o43222@gmail.com>
2022-12-24 10:13:37 +0800

    feat: add method-overrride

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+
 const app = express()
 const Restaurant = require('./models/restaurant') //載入model
-// const restaurant = require('./models/restaurant')
+
 const port = 3000
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -13,13 +16,13 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
 
 mongoose.set('strictQuery', false)
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-
-
 const db = mongoose.connection
+
 db.on('error', () => {
   console.log('mongodb error')
 })
@@ -46,7 +49,7 @@ app.get('/restaurant/:restaurant_id', (req, res) => {
     .then(item => res.render('detail', { item }))
     .catch(error => console.log(error))
 })
-// 修改特定資料
+// 修改特定資料 => 進入該頁面
 app.get('/restaurant/:restaurant_id/edit', (req, res) => {
   return Restaurant.findById(req.params.restaurant_id)
     .lean()
@@ -83,17 +86,17 @@ app.post('/restaurant', (req, res) => {
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
-app.post('/restaurant/:restaurant_id/edit', (req, res) => {
+app.put('/restaurant/:restaurant_id', (req, res) => {
   const id = req.params.restaurant_id
   const updateRestaurantData = req.body
   return Restaurant.findByIdAndUpdate(req.params.restaurant_id, updateRestaurantData)
     .then(() => res.redirect(`/restaurant/${id}`))
     .catch(error => console.log(error))
 })
-app.post('/restaurant/:restaurant_id/delete', (req, res) => {
+app.delete('/restaurant/:restaurant_id', (req, res) => {
   return Restaurant.findByIdAndDelete(req.params.restaurant_id, req.body)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
-app.listen(3000)
+app.listen(port)
